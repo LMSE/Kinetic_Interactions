8200407325abcc6d4ba2778a459e70862c42efa5
kiahdd <kianahaddadi@gmail.com>
2022-02-02 12:35:18 -0500

    Changed the algorithm to OOP

--- Code/connect.py
+++ Code/connect.py
@@ -1,104 +1,142 @@
 #%%
+# liberaries
 import mysql.connector
 from mysql.connector import errorcode
 import sys
-try:
-    cnx = mysql.connector.connect(user='***',password='***',host='***',database='***')
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
+import json
+import os
+from os.path import dirname, abspath
 
-cursor = cnx.cursor()
-# print(dir(cursor))
-# cnx.close()
+# defining variables
+input_folder = "\Input\\"
+file_name = "key.json"
+organism = 'escherichia coli'
+ec_number = "5.3.1.9"
+input_dir = dirname(dirname(abspath(__file__))) +input_folder
+key = input_dir + file_name
 
-# print(dir(cursor))
-# selecting target organism's cid and iid
-query = ("""select cid,iid, uid from main where uid in 
-(select refv from main where refv in (select uid from main 
-where cid =1 and refv = 0) and strv = %s)""")
-organism = ('escherichia coli',) # which organism is your target
-try:
-    cursor.execute(query, organism)
-    row = cursor.fetchone()
-except Exception as a:
-    print("Something is wrong in query one:")
-    print(a)
-    cnx.close()
+# defining functions
+# connect to mysql and return cursor
+def connect_to_mysql():
+    with open(key) as jsonfile:
+        data = json.load(jsonfile)
+    try:
+        cnx = mysql.connector.connect(user=data["user"],password=data["password"],host=data["host"],database=data["database"])
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    return cnx
 
-if row == None:
-    print("no results for organism {}".format(organism[0]))
-else:
-    organism_cid = row[0]
-    organism_iid = row[1]
-    organism_uid = row[2]
-    print("the organism has a cid of {} an iid of {} and a uid of {}".format(organism_cid,organism_iid, organism_uid))
+# defining classes
+class Organism:
+    def __init__(self, name, cid=[] ,iid=[],uid=[]):
+        self.name = name
+        self.cid = cid
+        self.iid = iid
+        self.uid = uid
+
+    def get_db_info(self,query, parameters):
+        res = []
+        self.cnx = connect_to_mysql()
+        cursor = self.cnx.cursor()
+        try:
+            cursor.execute(query, parameters)
+            row = cursor.fetchone()
+            while row != None:
+                res.append(row)
+                row = cursor.fetchone()
+            
+        except Exception as a:
+            print("Something is wrong in the query:")
+            print(a)
+            print("Mysql warnings:")
+            print(cursor._fetch_warnings())
+            print("executed query:")
+            print(cursor._executed)
+            self.cnx.close()
+
+        if not res:
+            print("no results for {}".format(self.name))
+            print("executed query:")
+            print(cursor._executed)
+        else:
+            print(res)
+            self.cid = [res[i][0] for i in range(len(res))]
+            self.iid = [res[i][1] for i in range(len(res))]
+            self.uid = [res[i][2] for i in range(len(res))]
+            print("cid = {} ,iid = {} and uid = {}".format(self.cid,self.iid, self.uid))
+
+    def close_connection(self):
+        self.cnx.close()
+        
+
+class EC_number(Organism):
+    def __init__(self, name, cid=[],iid=[],uid=[]):
+        super().__init__(name,cid,iid,uid)
+
+class Activator(Organism):
+    def __init__(self, name, cid=[],iid=[],uid=[]):
+        super().__init__(name,cid,iid,uid)
+
+    def get_db_info(self,query, parameters):
+        super().get_db_info(query, parameters)
+        self.name = "name"
+
+
+
+
+# %% EC query
+def main():
+    query = ("""select cid,iid, uid from main where uid in 
+    (select refv from main where refv in (select uid from main 
+    where cid =1 and refv = 0) and strv = %s)""")
+    parameter = (organism,)
+
+    # constructing object for organism
+    O_obj = Organism(organism)
+    O_obj.get_db_info(query,parameter)
+    O_obj.close_connection()
+
+    query = ("""select cid, iid, uid from main where 
+    uid in (select refv from main where refv in 
+    (select uid from main where cid = 2 and refv = 0) 
+    and iid = 17 and strv = %s)""")
+    parameter = (ec_number,)
+
+    ec_obj = EC_number(ec_number)
+    ec_obj.get_db_info(query,parameter)
+    ec_obj.close_connection()
+
+    # inhibitor uid
+
+    query = (
+    """(select cid,iid,uid from main where refv in 
+    (select uid from main where refv in 
+    (select uid from main where refv in 
+    (select uid from main where refv = %s and cid = 6 and iid = 1) 
+    and cid = %s and iid = %s) and cid = 6 and iid = 1))"""
+    )
+    parameter = (O_obj.uid[0],ec_obj.cid[0],ec_obj.iid[0])
+    print(parameter)
+    param_obj = Activator("test")
+    param_obj.get_db_info(query,parameter)
+    param_obj.close_connection()
+
+    # inhibitor name
+    query = (
+        """select strv from main where refv in 
+    (select uid from main where cid = %s and iid = %s and refv = 0) 
+    and cid = 5 and iid in (3,2,1) and row = 1
+    """
+    )
+
+main()
+#%%
+
+
+ 
     
-    
-# select target EC number's cid and iid
-cursor.reset()
-query = ("""select cid, iid, uid from main where 
-uid in (select refv from main where refv in 
-(select uid from main where cid = 2 and refv = 0) 
-and iid = 17 and strv = %s)""")
-ec_number = ("1.1.1.1",)
-try:
-    cursor.execute(query, ec_number)
-    row = cursor.fetchone()
-except Exception as A:
-    print("Something is wrong in query two:")
-    print(A)
-    cnx.close()
-if row == None:
-    print("no results for ec number {}".format(ec_number[0]))
-    cnx.close()
-    sys.exit()
-
-EC_number_cid = row[0]
-EC_number_iid = row[1]
-print("the target EC number has a cid of {} and iid of {}".format(EC_number_cid,EC_number_iid))
-    
-# fetch all compounds linked to the reaction in the specific organism
-cursor.reset()
-query = (
-"""(select cid,iid,uid from main where refv in 
-(select uid from main where refv in 
-(select uid from main where refv in 
-(select uid from main where refv = %s and cid = 6 and iid = 1) 
-and cid = %s and iid = %s) and cid = 6 and iid = 1))"""
- )
-try:
-    parameters = (organism_uid,EC_number_cid,EC_number_iid)
-    cursor.execute(query,params=parameters)
-    row = cursor.fetchone()
-    compound_cid=[]
-    compound_iid=[]
-    compound_uid =[]
-except Exception as B:
-    print("Something is wrong in query three:")
-    print(B)
-    cnx.close()
-while row != None:
-    print(row[0])
-    compound_cid.append(row[0])
-    compound_iid.append(row[1])
-    compound_uid.append(row[2])
-    row = cursor.fetchone()
-print("compounds with cid of {}, iid of {} and uid of {} are potential regulators".format(compound_cid,compound_iid,compound_uid))
-
-# obtaining the KI values for these compounds
-cursor.reset()
-cursor.execute("select floatV from main where refv in (%s) and iid = 18 and cid = 5" % ",".join(map(str,compound_uid)))
-# cursor._executed
-kinetic_param=[]
-row = cursor.fetchone()
-while row != None:
-    kinetic_param.append(row[0])
-    row = cursor.fetchone()
-print(kinetic_param)
-cnx.close()
-# %%
