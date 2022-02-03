42dd0259c12f7a99eded1b67edbaa712a32fbc8d
kiahdd <kianahaddadi@gmail.com>
2022-02-02 21:17:03 -0500

    cleared output. does not work in case we have multiple regulators. will edit it in the next commit

--- Code/Program_1.py
+++ Code/Program_1.py
@@ -3,18 +3,20 @@
 from matplotlib.font_manager import list_fonts
 import mysql.connector
 from mysql.connector import errorcode
-import sys
 import json
-import os
 from os.path import dirname, abspath
+import pandas as pd
+from pprint import pprint
 
 # defining variables
 input_folder = "\Input\\"
 file_name = "key.json"
 organism = 'escherichia coli'
 ec_number = "5.3.1.9"
-input_dir = dirname(dirname(abspath(__file__))) +input_folder
+dir = dirname(dirname(abspath(__file__))) 
+input_dir = dir + input_folder
 key = input_dir + file_name
+logfile = dir + "\log.txt"
 
 # defining functions
 # connect to mysql and return cursor
@@ -35,23 +37,24 @@ def connect_to_mysql():
 # defining classes
 class Organism:
     def __init__(self, name, cid=[] ,iid=[],uid=[], strv = [], comment = [], res=[]):
-        self.name = name
-        self.cid  = cid
-        self.iid  = iid
-        self.uid  = uid
-        self.strv = strv
-        self.res  = res
-        self.comment = comment
+        print("constucting {}".format(name))
+        self.name       = name
+        self.cid        = cid
+        self.iid        = iid
+        self.uid        = uid
+        self.strv       = strv
+        self.res        = res
+        self.comment    = comment
 
     def get_db_info(self,query, parameters):
-        self.cnx = connect_to_mysql()
-        cursor = self.cnx.cursor()
+        self.cnx        = connect_to_mysql()
+        cursor          = self.cnx.cursor()
         try:
             cursor.execute(query, parameters)
-            row = cursor.fetchone()
-            while row != None:
+            row         = cursor.fetchone()
+            while row  != None:
                 self.res.append(row)
-                row = cursor.fetchone()
+                row     = cursor.fetchone()
         except Exception as a:
             print("Something is wrong in the query:")
             print(a)
@@ -70,15 +73,13 @@ class Organism:
     def close_connection(self):
         self.cnx.close()
 
-    def load_results(self):
-        # print("loading results ...")
-        self.cid = [self.res[i][0] for i in range(len(self.res))]
-        self.iid = [self.res[i][1] for i in range(len(self.res))]
-        self.uid = [self.res[i][2] for i in range(len(self.res))]
-        # print("cid = {} ,iid = {} and uid = {}".format(self.cid,self.iid, self.uid))
+    def load_results_into_object(self):
+        self.cid        = [self.res[i][0] for i in range(len(self.res))]
+        self.iid        = [self.res[i][1] for i in range(len(self.res))]
+        self.uid        = [self.res[i][2] for i in range(len(self.res))]
 
-    def get_results(self):
-        print("obtained results are \n {}".format(self.res))
+    def print_results(self):
+        print("results for {} are: \n {}".format(self.name, self.res))
 
 class EC_number(Organism):
     def __init__(self, name, cid=[] ,iid=[],uid=[], strv = [], comment = [],res=[]):
@@ -88,41 +89,53 @@ class Activator(Organism):
     def __init__(self, name, cid=[] ,iid=[],uid=[], strv = [], comment = [], res=[]):
         super().__init__(name,cid,iid,uid,strv,comment,res)
 
-    def load_results(self):
-        super().load_results()
-        self.strv = [self.res[i][3] for i in range(len(self.res))]
-        self.comment = [self.res[i][4] for i in range(len(self.res))]
-        # print("strv = {} ".format(self.strv))
+    def load_results_into_object(self):
+        super().load_results_into_object()
+        self.strv       = [self.res[i][3] for i in range(len(self.res))]
+        self.comment    = [self.res[i][4] for i in range(len(self.res))]
     
-
-
+    def cleared_result(self):
+        myset = list(set(self.uid))
+        results         = {}
+        results["uid"]  = self.uid
+        results["cid"]  = self.cid
+        results["iid"]  = self.iid
+        results["strv"] = self.strv
+        results["lstrv"]= [len(item) for item in self.strv]
+        results["tag"]  = self.comment
+        lenmin          = min(results["lstrv"])
+        df = pd.DataFrame.from_dict(results)
+        res_df = pd.DataFrame(data = None, columns= df.columns)
+        for i in range(len(myset)):
+            
+            res_df = res_df.append(df.loc[ (df.uid==myset[i]) & (df.lstrv == lenmin)])
+        return res_df
 #  EC query
 def main():
     # constructing object for organism
-    print("constructing Organism Object ***")
     query = ("""select distinct cid,iid, uid from main where uid in 
     (select refv from main where refv in (select uid from main 
     where cid =1 and refv = 0) and strv = %s)""")
-    parameter = (organism,)
-    O_obj = Organism(organism)
+    parameter           = (organism,)
+    O_obj               = Organism(organism)
     O_obj.get_db_info(query,parameter)
-    O_obj.load_results()
+    O_obj.load_results_into_object()
+    O_obj.print_results()
     O_obj.close_connection()
     
     # constructing EC Object
-    print("constructing EC Object ***")
     query = ("""select distinct cid, iid, uid from main where 
     uid in (select refv from main where refv in 
     (select uid from main where cid = 2 and refv = 0) 
     and iid = 17 and strv = %s)""")
-    parameter = (ec_number,)
-    ec_obj = EC_number(ec_number)
+    parameter           = (ec_number,)
+    ec_obj              = EC_number(ec_number)
     ec_obj.get_db_info(query,parameter)
-    ec_obj.load_results()
+    ec_obj.load_results_into_object()
+    ec_obj.print_results()
     ec_obj.close_connection()
 
     # inhibitor uid
-    print("constructing Inhibitor Object ***")
     query = (
     """select distinct t1.cid,t1.iid,t1.uid, t3.strv,
     if(t4.iid=10,"Inhibitor",if(t4.iid=11,"Activator", if(t4.iid=12,"Cofactor","Else"))) 
@@ -136,18 +149,21 @@ def main():
     where t1.refv in 
     (select uid from main where refv in 
     (select uid from main where refv in 
-    (select uid from main where cid = 6 and iid = 1 and refv = %s) 
+    (select uid from main where cid = 6 and iid = 1 and refv = %s ) 
     and cid = %s and iid = %s) and cid = 6 and iid = 1)
     and t2.refv = 0 and t3.cid = 5 AND t3.iid in (1,2,3)
-    order by CHAR_LENGTH(t3.strv) ASC limit 1
+    order by CHAR_LENGTH(t3.strv) ASC
 """
     )
-    parameter = (O_obj.uid[0], ec_obj.cid[0],ec_obj.iid[0])
-    param_obj = Activator("activators")
-    param_obj.get_results()
+    parameter           = (O_obj.uid[0], ec_obj.cid[0],ec_obj.iid[0])
+    param_obj           = Activator("activators")
     param_obj.get_db_info(query,parameter)
-    param_obj.get_results()
-    param_obj.load_results()
+    param_obj.load_results_into_object()
+    param_obj.close_connection()
+    dic = param_obj.cleared_result()
+    print("run",file=open(logfile, "a"))
+    print(dic,file=open(logfile, "a"))
+    print(dic)
 
 main()
  
