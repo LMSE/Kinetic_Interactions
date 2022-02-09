8fe600c30e9335f3b947e4203631c82715cd2af6
kiahdd <kianahaddadi@gmail.com>
2022-02-09 16:15:15 -0500

    cleaning run.py

--- run.py
+++ run.py
@@ -1,16 +1,10 @@
 # %%
-import sys
-import os
-
 from package_1 import helpers as h1
 from package_1 import constants as c
 
 
 
 h1.generate_key()
-
-h1.analyze_regulator()
-
-sys.modules[__name__].__dict__.clear()
+a = h1.analyze_regulator()
 
 # %%
